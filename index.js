a6e06bb753aa1234edd24906f98322b943acc841
dhfernando <hdilshan.fernando11@gmail.com>
2021-01-04 15:44:27 +0530

    init project

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const usersRoutes = require('./routes/user.js')
+
+const url = 'mongodb://localhost/cruddb'
+
+const app = express()
+const PORT = 2000
+app.use(bodyParser.json())
+
+mongoose.connect(url , { useNewUrlParser : true ,  useUnifiedTopology: true })
+const con = mongoose.connection
+
+con.on('open' , ()=> console.log( 'Connected to DB' ))
+
+
+app.use('/user' , usersRoutes )
+
+app.get('/' , (req,res)=>{
+    res.send('Hello Express')
+})
+ 
+app.listen(PORT , ()=>{
+    console.log(`start api localhost:${PORT}`)
+})
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// import user functions
+const user = require('../controllers/user.js')
+
+// user routes
+
+router.get('/'  ,user.getUsers )
+router.post('/' , user.addUser )
+router.get('/:uuid' , user.getUser)
+router.delete('/:uuid' , user.deleteUser)
+
+
+module.exports = router 
+ 
